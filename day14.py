33f747c7b845bbcf2bffae56a47f89b600966bd0
Karel Crombecq <karel.crombecq@gmail.com>
2021-12-14 08:22:05 +0100

    day 14

--- day14.py
+++ day14.py
@@ -9,41 +9,7 @@ import numpy
 from collections import Counter
 
 
-def solve_1():
-    with open("input14-2.txt") as f:
-
-        lines = [line.strip("\n") for line in f.readlines()]
-
-        template = lines[0]
-
-        regex = re.compile(r"([A-Z]+) -> ([A-Z])")
-        rules = {}
-        for line in lines[2:]:
-            match = regex.match(line)
-            groups = match.groups()
-            rules[groups[0]] = groups[1]
-
-
-        steps = 10
-        for step in range(0, steps):
-            print(f"Step {step}:")
-            next_template = ""
-            for i in range(0, len(template)-1):
-                input = template[i:i+2]
-                output = rules[input]
-                next_template += input[0] + output
-
-            next_template += template[-1]
-
-            template = next_template
-
-        counter = Counter(template)
-        print(counter)
-        sorted = counter.most_common()
-        score = sorted[0][1] - sorted[-1][1]
-        print(f"The final score is {score}")
-
-def solve_2():
+def solve(steps):
     with open("input14-2.txt") as f:
 
         lines = [line.strip("\n") for line in f.readlines()]
@@ -67,11 +33,7 @@ def solve_2():
         print(rule_occurence)
         print(letter_occurence)
 
-
-        steps = 40
         for step in range(0, steps):
-            print(f"Step {step}:")
-
             next_rule_occurence = rule_occurence.copy()
             for input in rule_occurence:
                 n_rules_occurences = rule_occurence[input]
@@ -89,7 +51,7 @@ def solve_2():
 
         sorted = Counter(letter_occurence).most_common()
         score = sorted[0][1] - sorted[-1][1]
-        print(f"The final score is {score}")
+        print(f"The final score after {steps} steps is {score}")
 
 
 
@@ -99,4 +61,5 @@ def solve_2():
 
 
 
-solve_2()
+solve(10)
+solve(40)
